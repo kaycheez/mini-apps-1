1381e4edc549c8d8c9c740caf167ad13c197f37d
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-08-27 13:27:00 -0700

    implemented click function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,29 @@
 
+const choices = ['x', 'o'];
+let useX = true;
+let matrix = [['','',''],
+['','',''],
+['','','']];
+
+
+let onClick = function(event) {
+  if (useX) {
+    event.target.textContent = choices[0];
+  } else {
+    event.target.textContent = choices[1];
+  }
+  useX = !useX;
+};
+
+let placeTicOnClick = function() {
+  let cells = document.getElementsByTagName("td");
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].onclick = onClick.bind(cells[i]);
+  }
+};
+
+// let renderBoard = function() {
+
+// }
 
